1120778d0f0dca5a5803aacda0cfd4fbd405a0d0
Anna <58829319+AnnaKryvomlin@users.noreply.github.com>
2020-08-19 10:50:47 +0300

    stores

--- client-app/src/app/stores/gameStore.ts
+++ client-app/src/app/stores/gameStore.ts
@@ -1,3 +1,10 @@
+import { RootStore } from "./rootStore";
+
 export default class GameStore{
-    
+    rootStore: RootStore;
+
+    constructor(rootStore: RootStore) {
+        this.rootStore = rootStore;
+    }
+
 }
--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -1,14 +1,17 @@
 import UserStore from './userStore';
 import { createContext } from 'react';
 import CommonStore from './commonStore';
+import GameStore from './gameStore';
 
 export class RootStore {
     userStore: UserStore;
     commonStore: CommonStore;
+    gameStore: GameStore;
 
     constructor() {
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
+        this.gameStore = new GameStore(this);
     }
 }
 
